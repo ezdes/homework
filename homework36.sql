8878a8c91239437f54dacca854c4bca7f8526811
ezdes <ermekxt22@gmail.com>
2021-02-08 08:08:13 +0600

    sql

---
+++ homework36.sql
@@ -0,0 +1,68 @@
+--Найти всех клиентов, которые в одной из стран: Испания, Италия, Германия, Франция. Отсортируйте по странам
+select * from customer
+where country in('Spain', 'Italy', 'Germany', 'France')
+order by country;
+
+--Найти всех клиентов, имена которых начинаются на ‘Jo’
+select * from customer
+where first_name like 'Jo%';
+
+--Найти всех клиентов, имена которых заканчиваются на ‘a’ и имеют длину ровно 4 символа
+select * from customer
+where first_name like '%a' and length(first_name) = 4;
+
+--Найти количество клиентов по странам
+select count(id) as amount, country
+from customer
+group by country;
+
+--Найти количество клиентов по странам. Вывести в порядке убывания
+select count(id) as amount, country
+from customer
+group by country
+order by count(id) desc;
+
+--Найти общую сумму стоимости заказов и количество заказов по каждому клиенту (customer_id). Отсортировать по сумме
+select customer.id, customer.first_name, round(sum(orders.total_amount), 2) as sum, count(orders.customer_id) as amount
+from orders
+join customer on customer.id = orders.customer_id
+group by customer.id
+order by sum(orders.total_amount);
+
+--Найти общую сумму стоимости заказов и количество заказов по каждому клиенту (customer_id), у которых общее количество заказов больше 20ти
+select customer.id, customer.first_name, round(sum(orders.total_amount), 2) as sum, count(orders.customer_id) as amount
+from orders
+join customer on customer.id = orders.customer_id
+group by customer.id, customer.first_name
+having count(orders.customer_id) > 20;
+
+--Найти количество городов в каждой из стран клиентов
+select country, count(city) as amount
+from customer
+group by country;
+
+--Выбрать: К какому заказу(номер заказа), Как зовут клиента, Из какого он города, Наименование товара, Цена товара, Название поставщика
+select o.id, o.order_number, c.first_name, c.city, p.product_name, p.unit_price, s.contact_name
+from orders o
+join customer c on o.customer_id = c.id
+join order_item oi on oi.order_id = o.id
+join product p on p.id = oi.product_id
+join supplier s on s.id = p.supplier_id;
+
+--Узнать есть ли поставщики без товара
+select s.id, s.contact_name
+from supplier s
+left join product p on p.supplier_id = s.id
+where p.supplier_id is null;
+
+--Выбрать только те товары которые были заказаны
+select distinct p.id, p.product_name
+from product p 
+join order_item oi on oi.product_id = p.id
+order by p.id asc;
+
+--Выбрать только те товары которые не были заказаны
+select p.id, p.product_name
+from product p 
+left join order_item oi on oi.product_id = p.id
+where oi.product_id is null;
---
+++ model.sql
@@ -0,0 +1,45 @@
+create table customer (
+   id                   int                  primary key,
+   first_name           varchar(100)         not null,
+   last_name            varchar(100)         not null,
+   city                 varchar(100)         null,
+   country              varchar(100)         null,
+   phone                varchar(100)         null
+
+);
+
+create table supplier (
+   id                    int                  primary key,
+   company_name          varchar(100)         not null,
+   contact_name          varchar(100)         null,
+   contact_title         varchar(100)         null,
+   city                  varchar(100)         null,
+   country               varchar(100)         null,
+   phone                 varchar(100)         null,
+   fax                   varchar(100)         null
+);
+
+create table product (
+   id                    int                  primary key,
+   product_name          varchar(100)         not null,
+   unit_price            decimal(12,2)        null default 0,
+   package               varchar(100)         null,
+   is_discontinued       boolean              not null default false,
+   supplier_id           int                  references supplier(id) not null
+);
+
+create table orders (
+   id                    int                  primary key,
+   order_date            timestamp            not null default now(),
+   order_number          varchar(100)         null,
+   total_amount          decimal(12,2)        null default 0,
+   customer_id           int                  references customer(id) not null
+);
+
+create table order_item (
+   id                    int                  primary key,
+   unit_price            decimal(12,2)        not null default 0,
+   quantity              int                  not null default 1,
+   order_id              int                  references orders(id) not null,
+   product_id            int                  references product(id) not null
+);
