af4b57ae0c7aebd1af4fa42d5b8f828e95c734cf
ezdes <71791856+ezdes@users.noreply.github.com>
2021-03-17 20:28:12 +0600

    Update SumService.java

--- homework48/src/main/java/Service/SumService.java
+++ homework48/src/main/java/Service/SumService.java
@@ -10,7 +10,7 @@ public class SumService {
     @GET
     @Path("/{num1}/{num2}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Result sum(@PathParam("num1") Integer num1, @PathParam("num2") Integer num2) {
+    public Result sum(@PathParam("num1") Integer num1, @PathParam("num2") Integer num2) { //if you return an Integer, it throws an error!!!
         return new Result(num1 + num2);
     }
 }
